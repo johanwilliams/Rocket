6f280602072a0d7322a4a48f8ce77d42ded6e666
johan <williams.johan@gmail.com>
2022-01-23 23:19:18 +0100

    Replaced the NetworkManager with own class and did some tutorial on data syncronization

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    #region Server System Callbacks
+
+    /// <summary>
+    /// Called on the server when a client is ready.
+    /// <para>The default implementation of this function calls NetworkServer.SetClientReady() to continue the network setup process.</para>
+    /// </summary>
+    /// <param name="conn">Connection from client.</param>
+    public override void OnServerReady(NetworkConnection conn)
+    {
+        base.OnServerReady(conn);
+        Debug.Log("A client is ready");
+    }
+
+    /// <summary>
+    /// Called on the server when a client disconnects.
+    /// <para>This is called on the Server when a Client disconnects from the Server. Use an override to decide what should happen when a disconnection is detected.</para>
+    /// </summary>
+    /// <param name="conn">Connection from client.</param>
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {        
+        base.OnServerDisconnect(conn);
+        Debug.Log("Client disconnected");
+    }
+
+    #endregion
+
+    #region Client System Callbacks
+
+    /// <summary>
+    /// Called on the client when connected to a server.
+    /// <para>The default implementation of this function sets the client as ready and adds a player. Override the function to dictate what happens when the client connects.</para>
+    /// </summary>
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        Debug.Log("Connected to the server!");
+    }
+
+    /// <summary>
+    /// Called on clients when disconnected from a server.
+    /// <para>This is called on the client when it disconnects from the server. Override this function to decide what happens when the client disconnects.</para>
+    /// </summary>
+    public override void OnClientDisconnect()
+    {        
+        base.OnClientDisconnect();
+        Debug.Log("Disconnected from the server!");
+    }
+
+    #endregion
+
+    #region Start & Stop Callbacks
+
+    /// <summary>
+    /// This is invoked when a server is started - including when a host is started.
+    /// <para>StartServer has multiple signatures, but they all cause this hook to be called.</para>
+    /// </summary>
+    public override void OnStartServer()
+    {
+        Debug.Log("Server started!");
+    }
+
+    /// <summary>
+    /// This is invoked when the client is started.
+    /// </summary>
+    public override void OnStartClient()
+    {
+        Debug.Log("Client started!");
+    }
+
+    /// <summary>
+    /// This is called when a server is stopped - including when a host is stopped.
+    /// </summary>
+    public override void OnStopServer()
+    {
+        Debug.Log("Server stopped!");
+    }
+
+    /// <summary>
+    /// This is called when a client is stopped.
+    /// </summary>
+    public override void OnStopClient()
+    {
+        Debug.Log("Client stopped!");
+    }
+
+    #endregion
+
+
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
+
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    int holaCount = 0;
+
     void HandleMovement()
     {
         if (isLocalPlayer)
@@ -19,5 +23,38 @@ public class Player : NetworkBehaviour
     private void Update()
     {
         HandleMovement();
+
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Sending Hola to the server");
+            Hola();
+        }
+
+       
+    }
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received Hola from client!");
+        holaCount++;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Received Hola from server");
+    }
+
+    [ClientRpc]
+    void TooHigh()
+    {
+        Debug.Log("Too high!");
+    }
+
+    void OnHolaCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"Hola count changed from {oldCount} to {newCount}");
     }
 }
